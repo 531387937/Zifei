4e677e3bffafe1a3e30b20eb6f4186556efed926
QiZi <531387937@qq.com>
2019-04-13 17:34:47 +0800

    移动平台

---
+++ ZiFei U2017.4.16/Assets/Scripts/LevelOne/LevelOneMovePlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneMovePlatform : MonoBehaviour
+{
+    private Animator an;
+    private bool down = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        an = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Hero"&&!down)
+        {
+            Invoke("MovePlat", 0.5f);
+            down = true;
+        }
+    }
+    void MovePlat()
+    {
+        down = false;
+        an.SetTrigger("Down");
+    }
+}
